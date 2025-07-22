896f318f2125d3e03ac301035b5d94f6ea0b8d43
Artem <nukonopov@gmail.com>
2025-07-22 10:29:46 +0300

    write import libraries

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print('a')
+#base:
+import numpy as np
+import pandas as pd
+import random
+
+#vizualization:
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#NeuralNetworks
+import torch.nn as nn 
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torch.optim as optim
+
+#metrics, etc
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import f1_score
+from PIL import Image
+
+print("adfa")
